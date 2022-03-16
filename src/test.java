744429cdc7e4c9462b6ef705ba75c998f516f8cb
Konstantin Ehmann <konstantin.ehmann@outlook.de>
2022-03-16 09:43:17 +0100

    lel

--- src/test.java
+++ src/test.java
@@ -1,15 +1,10 @@
 import javax.swing.*;
 
 public class test {
-    private JButton button1;
-    private JPanel panel1;
-    private JPasswordField passwordField1;
-    private JSlider slider1;
-    private JFrame jFrame;
-
-    public test() {
-        button1 = new JButton();
-        panel1 = new JPanel();
-        jFrame = new JFrame();
+    private JFrame window;
+    public test(){
+        window = new JFrame("TEST");
+        window.pack();
+        window.setVisible(true);
     }
 }
