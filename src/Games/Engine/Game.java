4787b2ace4b7ba910db93807ade1a59db210a051
Konstantin Ehmann <konstantin.ehmann@outlook.de>
2022-03-17 12:26:16 +0100

    added some things

--- src/Games/Engine/Game.java
+++ src/Games/Engine/Game.java
@@ -1,8 +1,15 @@
 package Games.Engine;
 
-public abstract class Game {
-    Window window;
-    public Game(Window window){
+import javax.swing.*;
+import java.awt.*;
 
+public abstract class Game {
+    private Window window;
+    private JPanel surface;
+    public Game(Window window){
+        this.window = window;
+        this.surface = new JPanel();
+        this.surface.setBackground(Color.green);
+        window.add(this.surface);
     }
 }
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,10 +1,22 @@
 import Games.Engine.Window;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class MainMenu {
-
     private Window window;
-
+    private JPanel surface;
     public MainMenu(Window window){
         this.window = window;
+        surface = new JPanel();
+        surface.setBackground(Color.green);
+        surface.setSize(600, 600);
+        window.add(surface);
+
+        JButton b = new JButton();
+        b.setText("TestKnopfFürLevelAuswahl");
+        
+
+        window.add(b);
     }
 }
