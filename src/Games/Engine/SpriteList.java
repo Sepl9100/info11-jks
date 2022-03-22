be0675ab47aac6db4ed728adf1f6b173af55daeb
Konstantin Ehmann <konstantin.ehmann@outlook.de>
2022-03-22 08:28:18 +0100

    added Spritelist

---
+++ src/Games/Engine/SpriteList.java
@@ -0,0 +1,13 @@
+package Games.Engine;
+
+public class SpriteList {
+    public Sprite[][] list;
+    public SpriteList(){
+        list = new Sprite[1][1]; //Sprite[layer][place]
+    }
+
+    public void add_sprite(Sprite sprite, int layer){
+        list
+    }
+
+}
