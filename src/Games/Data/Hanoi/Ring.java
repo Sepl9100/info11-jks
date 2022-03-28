a7e9d64505a2451dbe23a18eeec573cf10981742
Sebastian R <sebastian.regensburg05@gmail.com>
2022-03-28 11:13:07 +0200

    wip hanoi

---
+++ src/Games/Data/Hanoi/Ring.java
@@ -0,0 +1,15 @@
+package Games.Data.Hanoi;
+
+import Games.Engine.Entity;
+import Games.Engine.Game;
+
+public class Ring {
+    // Size Number. 1-6. 6 = smallest
+    public int x, y, number;
+    private Game game;
+    private Entity ring_entity;
+
+    public Ring(Game game, int x, int y, int number){
+
+    }
+}
--- src/Games/Data/Hanoi/Stand.java
+++ src/Games/Data/Hanoi/Stand.java
@@ -8,23 +8,24 @@ import java.awt.*;
 
 public class Stand {
 
-    public int x, y;
+    public int x, y, size;
     private Game game;
     private Sprite rec_bottom, rec_top;
 
-    public Stand(Game game, int x, int y){
+    public Stand(Game game, int x, int y, int size){
         this.x = x;         // cords of the top left corner
         this.y = y;         // from the bottom rectangle
+        this.size = size;
         this.game = game;
 
         rec_bottom = new Sprite(this.game, 5, null);
         rec_bottom.set_pos(x, y);
-        rec_bottom.resize(240, 20);
+        rec_bottom.resize(24*size, 2*size);
         rec_bottom.color = Color.white;
 
         rec_top = new Sprite(this.game, 5, null);
         rec_top.set_pos(x+110, y-240);
-        rec_top.resize(20, 240);
+        rec_top.resize(2*size, 24*size);
         rec_top.color = Color.white;
 
     }
