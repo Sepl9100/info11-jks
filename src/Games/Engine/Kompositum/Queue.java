027bb2b18f34579389ba980ca98d094fd13ec022
Sebastian R <sebastian.regensburg05@gmail.com>
2022-03-29 08:41:34 +0200

    (wip) queue

--- src/Games/Engine/Kompositum/Queue.java
+++ src/Games/Engine/Kompositum/Queue.java
@@ -1,4 +1,17 @@
 package Games.Engine.Kompositum;
 
 public class Queue {
+    private Listelement first;
+
+    public Queue() {
+        first = new End();
+    }
+
+    public void insert(Dataelement content){
+        first=first.insert(content);
+    }
+    
+    public int count_nodes() {
+        return first.count_nodes();
+    }
 }
