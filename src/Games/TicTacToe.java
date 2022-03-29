9be224aba2214c22b841a20fa98dc5c4f4f33d70
jans <sinusnatter@protonmail.com>
2022-03-29 08:45:44 +0200

    Protokoll und Anfang vom TicTacToe

--- src/Games/TicTacToe.java
+++ src/Games/TicTacToe.java
@@ -4,6 +4,7 @@ import Games.Engine.Game;
 import Games.Engine.Sprite;
 import Games.Engine.Window;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
@@ -15,6 +16,17 @@ public class TicTacToe extends Game {
     public TicTacToe(Window window){
         super(window, "Tic-Tac-Toe");
         this.setBackground(Color.gray);
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(3,3,3,3);
+
+        JLabel topText = new JLabel();
+        topText.setText("Hier ist der Obere Text \n Hier ist Die zweite Reihe an Text");
+        gbc.gridwidth= 11;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        this.add(topText, gbc);
+
         window.pack();
     }
 
