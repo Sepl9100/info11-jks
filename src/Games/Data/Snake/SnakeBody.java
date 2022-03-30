3272ecdf4b544ab7157208132f2faa2a42cc7176
Konstantin Ehmann <konstantin.ehmann@outlook.de>
2022-03-30 10:08:57 +0200

    wip snake

---
+++ src/Games/Data/Snake/SnakeBody.java
@@ -0,0 +1,29 @@
+package Games.Data.Snake;
+
+import Games.Snake;
+
+import java.awt.image.BufferedImage;
+
+public class SnakeBody extends SnakeGameSprite{
+
+    private SnakeBody next_body;
+
+    public SnakeBody(Snake game, BufferedImage image){
+        super(game, 6, image);
+        next_body = new SnakeBody(game, image);
+
+    }
+
+    public void add_body(){
+        if (next_body == null){
+            next_body = new SnakeBody(game, image);
+        }
+    }
+
+    public void move(int x, int y) {
+        if (next_body != null) {
+            next_body.set_pos((int) this.x, (int) this.y);
+        }
+        super.move(x, y);
+    }
+}
