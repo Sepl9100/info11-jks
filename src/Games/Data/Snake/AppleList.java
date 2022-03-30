45b46fef0020eb417158f8a58cf45dc448178929
Olikonsti <konstantin.ehmann@outlook.de>
2022-03-30 22:36:01 +0200

    fixed sprite list and apple list remove functions

--- src/Games/Data/Snake/AppleList.java
+++ src/Games/Data/Snake/AppleList.java
@@ -15,15 +15,21 @@ public class AppleList {
     }
 
     public void remove_sprite(Apple apple){
+        boolean removed = false;
             for (int index = 0; index < filled_until; index++) {
                 Apple element = list[index];
                 if (element == apple) {
-                    for (int index2 = index; index2 < filled_until - 1; index2++) {
+                    for (int index2 = index; index2 < filled_until; index2++) {
                         Apple next_element = list[index2 + 1]; // sprite list elemente alle 1 in richtung anfang bewegen
                         list[index2] = next_element;
                     }
+                    removed = true;
                 }
             }
+            if (removed){
+                list[filled_until+1] = null;
+            }
+
     }
 
     public void print(){
--- src/Games/Engine/SpriteList.java
+++ src/Games/Engine/SpriteList.java
@@ -13,18 +13,25 @@ public class SpriteList {
     }
 
     public void remove_sprite(Sprite sprite){
+        boolean removed = false;
         for (Sprite[] layer : list){
             for (int index = 0; index < filled_until; index++){
                 Sprite element = layer[index];
                 if (element == sprite){
-                    for (int index2 = index; index2 < filled_until-1; index2++)
+                    for (int index2 = index; index2 < filled_until; index2++)
                     {
                         Sprite next_element = layer[index2+1]; // sprite list elemente alle 1 in richtung anfang bewegen
                         layer[index2] = next_element;
                     }
+                    removed = true;
                 }
             }
+            if (removed){
+                layer[filled_until+1] = null;
+            }
+
         }
+
     }
 
     public void print(){
