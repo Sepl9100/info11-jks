5b58f7f83ba33ea32ea5473afd276d950cc982b2
Sebastian R <sebastian.regensburg05@gmail.com>
2022-04-06 12:00:21 +0200

    Create Sudoku.java

---
+++ src/Games/Sudoku.java
@@ -0,0 +1,16 @@
+package Games;
+
+import Games.Engine.Game;
+import Games.Engine.Window;
+
+public class Sudoku extends Game {
+
+    public Sudoku(Window window){
+        super(window, "Sudoku");
+    }
+
+    @Override
+    public void update_loop() {
+
+    }
+}
