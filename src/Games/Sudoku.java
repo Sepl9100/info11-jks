8397ea327593a979e1fdfc2cfe51c75f0245933c
Sebastian R <sebastian.regensburg05@gmail.com>
2022-04-28 12:17:14 +0200

    wip - sudoku
    added font for tic tac toe

--- src/Games/Sudoku.java
+++ src/Games/Sudoku.java
@@ -1,12 +1,30 @@
 package Games;
 
+import Games.Data.Sudoku.Grid;
 import Games.Engine.Game;
 import Games.Engine.Window;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Sudoku extends Game {
+
+    public GridBagConstraints gbc;
+
+    private Grid grid;
 
     public Sudoku(Window window){
         super(window, "Sudoku");
+
+        this.setBackground(Color.gray);
+        this.setLayout(new GridBagLayout());
+        gbc = new GridBagConstraints();
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(1, 1, 1, 1);
+        gbc.ipadx = 125;
+        gbc.ipady = 25;
+
+        grid = new Grid(this, 0, 0);
     }
 
     @Override
