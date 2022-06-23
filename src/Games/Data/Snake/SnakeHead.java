2f419d36df84631a78cf9ba45ff37faff9886085
Konstantin Ehmann <konstantin.ehmann@outlook.de>
2022-06-23 12:06:54 +0200

    Snake code refactor

--- src/Games/Data/Snake/SnakeHead.java
+++ src/Games/Data/Snake/SnakeHead.java
@@ -10,9 +10,7 @@ import java.awt.image.BufferedImage;
 public class SnakeHead extends SnakeGameSprite{
 
     private SnakeBody next_body;
-
     public boolean on_drugs;
-
     private BufferedImage image_body;
 
     public SnakeHead(Snake game, BufferedImage image_head, BufferedImage image_body){
@@ -34,20 +32,19 @@ public class SnakeHead extends SnakeGameSprite{
     public void move(int x, int y) {
         if (!check_collision(tile_x + x, tile_y + y)){
             if (next_body != null) {
-                next_body.follow_snake((int) tile_x, (int) tile_y);
+                next_body.follow_snake((int) tile_x, (int) tile_y);     // Snake body pieces recursiv mitbewegen
             }
-            SnakeGameSprite apple = game.check_apple(tile_x + x, tile_y + y);
+            SnakeGameSprite apple = game.check_apple(tile_x + x, tile_y + y);       // wenn vor apfel
             if (apple != null){
-                this.add_body();
+                this.add_body();        // schlange verlängern
                 game.score += 1;
                 apple.delete();
                 game.apples.remove_sprite(apple);
                 game.place_apple();
             }
-
-            SnakeGameSprite drug = game.check_drug(tile_x + x, tile_y + y);
+            SnakeGameSprite drug = game.check_drug(tile_x + x, tile_y + y);         // wenn vor doping
             if (drug != null){
-                this.add_body();
+                this.add_body();        // schlange verlängern
                 game.score += 10;
                 drug.delete();
                 game.drugs.remove_sprite(drug);
@@ -56,10 +53,8 @@ public class SnakeHead extends SnakeGameSprite{
                 new QueueTask(game, 200, e -> this.on_drugs = false); // Task erstellen -> Spieler nach 200 ticks wieder langsam machen
             }
             super.move(x, y);
-
         }
         else game.game_over_screen();
-
     }
 
     public boolean check_collision(int tilex, int tiley){
